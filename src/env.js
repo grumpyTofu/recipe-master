bf36806b92f407b65a62ef8dab7d662e819dd159
afelix <austinjfelix@gmail.com>
2020-07-25 23:57:09 -0400

    stop tracking changes to env.js

--- src/env.js
+++ src/env.js
@@ -3,7 +3,7 @@
 
     // API url
     window.__env.apiUrl = 'https://tasty.p.rapidapi.com/recipes';
-    window.__env.apiKey = 'ac3f29632amsha9febd588cd9ff4p11d433jsn949d54afb087';
+    window.__env.apiKey = '';
     window.__env.apiHost = 'tasty.p.rapidapi.com';
 
     // Whether or not to enable debug mode
